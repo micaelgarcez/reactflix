66420e80cfa42133efc9f111ace3450d6967745c
Micael Garcez <micaelgarcez@outlook.com>
2020-07-29 13:03:16 -0300

    aula02

--- src/index.js
+++ src/index.js
@@ -1,11 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
+import Home from "./pages/Home";
+import CadastroVideo from "./pages/cadastro/Video";
+import CadastroCategoria from "./pages/cadastro/Categoria";
+
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" component={Home} exact />
+      <Route path="/cadastro/video" component={CadastroVideo} exact />
+      <Route path="/cadastro/categoria" component={CadastroCategoria} exact />
+      <Route
+        component={() => (
+          <div>
+            <h1>Página 404</h1>
+          </div>
+        )}
+      />
+    </Switch>
+  </BrowserRouter>,
   document.getElementById("root")
 );
