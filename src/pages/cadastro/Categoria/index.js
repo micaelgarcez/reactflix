a87f102b27a34338a218c2186e3b0fcc0cd85503
Micael Garcez <micaelgarcez@outlook.com>
2020-07-29 15:45:54 -0300

    aula03

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,20 +1,75 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import PageDefault from "../../../components/PageDefault";
+import FormField from "../../../components/FormField";
 
 function CadastroCategoria() {
+  const [categorias, setCategorias] = useState([]);
+
+  const valoresIniciais = {
+    nome: "",
+    descricao: "",
+    cor: "",
+  };
+
+  const [values, setValues] = useState(valoresIniciais);
+
+  function setValue(chave, valor) {
+    setValues({
+      ...values,
+      [chave]: valor,
+    });
+  }
+
+  function handleChange(e) {
+    const { getAttribute, value } = e.target;
+    setValue(e.target.getAttribute("name"), value);
+  }
+
   return (
     <PageDefault>
       <h1>Cadastro de Categoria</h1>
 
-      <form>
-        <label>
-          Nome da Categoria:
-          <input type="text" />
-        </label>
+      <form
+        onSubmit={function handleSubmit(e) {
+          e.preventDefault();
+          setCategorias([...categorias, values]);
+
+          setValues(valoresIniciais);
+        }}
+      >
+        <FormField
+          label="Nome:"
+          type="text"
+          name="nome"
+          value={values.nome}
+          onChange={handleChange}
+        />
+
+        <FormField
+          label="Descrição:"
+          type="text"
+          name="descricao"
+          value={values.descricao}
+          onChange={handleChange}
+        />
+
+        <FormField
+          label="Cor:"
+          type="color"
+          name="cor"
+          value={values.cor}
+          onChange={handleChange}
+        />
 
         <button>Cadastrar</button>
       </form>
+
+      <ul>
+        {categorias.map((categoria, index) => (
+          <li key={`${categoria.nome}${index}`}>{categoria.nome}</li>
+        ))}
+      </ul>
 
       <Link to="/">Ir para home</Link>
     </PageDefault>
