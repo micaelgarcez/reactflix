d00a62d358792ef9edc66c4b9e479edc9b88b04d
Micael Garcez <micaelgarcez@outlook.com>
2020-07-30 12:32:54 -0300

    aula04

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,15 +1,16 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
-import PageDefault from "../../../components/PageDefault";
-import FormField from "../../../components/FormField";
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import PageDefault from '../../../components/PageDefault';
+import FormField from '../../../components/FormField';
+import Button from '../../../components/Button';
 
 function CadastroCategoria() {
   const [categorias, setCategorias] = useState([]);
 
   const valoresIniciais = {
-    nome: "",
-    descricao: "",
-    cor: "",
+    nome: '',
+    descricao: '',
+    cor: '',
   };
 
   const [values, setValues] = useState(valoresIniciais);
@@ -23,8 +24,17 @@ function CadastroCategoria() {
 
   function handleChange(e) {
     const { getAttribute, value } = e.target;
-    setValue(e.target.getAttribute("name"), value);
+    setValue(e.target.getAttribute('name'), value);
   }
+
+  useEffect(() => {
+    const URL = 'http://localhost:8080/categorias';
+
+    fetch(URL).then(async (res) => {
+      const data = await res.json();
+      setCategorias([...data]);
+    });
+  }, []);
 
   return (
     <PageDefault>
@@ -39,7 +49,7 @@ function CadastroCategoria() {
         }}
       >
         <FormField
-          label="Nome:"
+          label="Nome"
           type="text"
           name="nome"
           value={values.nome}
@@ -47,27 +57,29 @@ function CadastroCategoria() {
         />
 
         <FormField
-          label="Descrição:"
-          type="text"
+          label="Descrição"
+          type="textarea"
           name="descricao"
           value={values.descricao}
           onChange={handleChange}
         />
 
         <FormField
-          label="Cor:"
+          label="Cor"
           type="color"
           name="cor"
           value={values.cor}
           onChange={handleChange}
         />
 
-        <button>Cadastrar</button>
+        <Button>Cadastrar</Button>
       </form>
+
+      {categorias.length === 0 && <div>Carregando...</div>}
 
       <ul>
         {categorias.map((categoria, index) => (
-          <li key={`${categoria.nome}${index}`}>{categoria.nome}</li>
+          <li key={`${categoria.nome}${index}`}>{categoria.titulo}</li>
         ))}
       </ul>
 
